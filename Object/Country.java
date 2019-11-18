26059d02b46db04822217c8d113e1207ccc0e429
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-18 23:28:23 +0700

    Update Country.java

--- Object/Country.java
+++ Object/Country.java
@@ -1,7 +1,12 @@
 
 public class Country {
-public Country() {
+	
+public Country(String dinhDanh,String nhan,String moTa) {
 	super();
+	this.dinhDanh=dinhDanh;
+	this.nhan=nhan;
+	this.moTa=moTa;
+	
 }
 
 }
