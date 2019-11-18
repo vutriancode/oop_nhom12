d7dd84f259397ac83800f3642305d238194a38cd
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-18 23:24:05 +0700

    Update Country.java

--- Object/Country.java
+++ Object/Country.java
@@ -1,34 +1,7 @@
 
-public class Country {private String dinhDanh;
-private String nhan;
-private String moTa;
-
+public class Country {
 public Country() {
-	
-}
-
-public String getDinhDanh() {
-	return dinhDanh;
-}
-
-public void setDinhDanh(String dinhDanh) {
-	this.dinhDanh = dinhDanh;
-}
-
-public String getNhan() {
-	return nhan;
-}
-
-public void setNhan(String nhan) {
-	this.nhan = nhan;
-}
-
-public String getMoTa() {
-	return moTa;
-}
-
-public void setMoTa(String moTa) {
-	this.moTa = moTa;
+	super();
 }
 
 }
