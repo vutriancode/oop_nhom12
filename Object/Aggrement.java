572db37c5a18cc0f1561d59915d00937db9104a6
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-18 23:14:57 +0700

    Update Aggrement.java

--- Object/Aggrement.java
+++ Object/Aggrement.java
@@ -1,37 +1,7 @@
 
-public class Aggrement {
-	
-private String dinhDanh;
-private String nhan;
-private String moTa;
-
-public Aggrement() {
-	
+public class Aggrement extends Kethuathuoctinhchung {
+	public Aggrement() {
+	super();
+}
 }
 
-public String getDinhDanh() {
-	return dinhDanh;
-}
-
-public void setDinhDanh(String dinhDanh) {
-	this.dinhDanh = dinhDanh;
-}
-
-public String getNhan() {
-	return nhan;
-}
-
-public void setNhan(String nhan) {
-	this.nhan = nhan;
-}
-
-public String getMoTa() {
-	return moTa;
-}
-
-public void setMoTa(String moTa) {
-	this.moTa = moTa;
-}
-
-
-}
