5e310133a893723b1159335ae287718fc723748a
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-18 23:12:00 +0700

    update

---
+++ Object/Kethuathuoctinhchung.java
@@ -0,0 +1,30 @@
+public class Kethuathuoctinhchung {
+	private String dinhDanh;
+	private String nhan;
+	private String moTa;
+	public String getDinhDanh() {
+		return dinhDanh;
+	}
+
+	public void setDinhDanh(String dinhDanh) {
+		this.dinhDanh = dinhDanh;
+	}
+
+	public String getNhan() {
+		return nhan;
+	}
+
+	public void setNhan(String nhan) {
+		this.nhan = nhan;
+	}
+
+	public String getMoTa() {
+		return moTa;
+	}
+
+	public void setMoTa(String moTa) {
+		this.moTa = moTa;
+	}
+
+
+	}
