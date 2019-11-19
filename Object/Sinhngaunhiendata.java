e6841289dfb07b101bcde93e82d2d46bc59515b4
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-19 10:12:55 +0700

    Delete Sinhngaunhiendata.java

--- Object/Sinhngaunhiendata.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-public class Sinhngaunhiendata {
-	ArrayList<String> dinhDanh=new ArrayList<String>();
-	ArrayList<String> nhan =new ArrayList<String>();
-	ArrayList<String> moTa=new ArrayList<String>();
-	
-
-}
-
