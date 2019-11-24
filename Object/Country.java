c2caf7b08890171714b8ae858eae623ccc273842
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-24 12:19:29 +0700

    Update Country.java

--- Object/Country.java
+++ Object/Country.java
@@ -1,13 +1,21 @@
 
-public class Country extends Kethuathuoctinhchung{
-	public String chauLuc ;
+public class Country extends Kethuathuoctinhchung {
+	public String chauLuc;
 	
-public Country(String dinhDanh,String nhan,String moTa) {
-	super();
-	this.dinhDanh=dinhDanh;
-	this.nhan=nhan;
-	this.moTa=moTa;
-	
-}
+
+	public Country(String dinhDanh,String nhan,String moTa) {
+		super();
+		this.dinhDanh=dinhDanh;
+		this.nhan=nhan;
+		this.moTa=moTa;
+		
+	}
+	public String getChauLuc() {
+		return chauLuc;
+	}
+
+	public void setChauLuc(String chauLuc) {
+		this.chauLuc = chauLuc;
+	}
 
 }
