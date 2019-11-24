72095e7a532d988152166277b42cd3ea06eea248
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-24 12:28:02 +0700

    Update Time.java

--- Object/Time.java
+++ Object/Time.java
@@ -1,4 +1,11 @@
 public class Time extends Kethuathuoctinhchung {
+	public int nam ;
+	public int thang ; 
+	public int ngay ;
+	public int gio ; 
+	public int phut ; 
+	public int giay ;
+	
 	public Time (String dinhDanh,String nhan,String moTa) {
 		super();
 		this.dinhDanh=dinhDanh;
