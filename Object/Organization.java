59ba95852cf086fe765e444dad31ce69fb8c3629
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-24 12:42:51 +0700

    Update Organization.java

--- Object/Organization.java
+++ Object/Organization.java
@@ -1,4 +1,4 @@
-public class Organization extends Object{
+public class Organization extends Thucthe{
 	public String truSo;
 	public Organization (String dinhDanh,String nhan,String moTa) {
 		super();
