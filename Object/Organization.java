cf853e434671e12396b68cd3c46a9490f8cf4212
tranbamanh229289 <55541720+tranbamanh229289@users.noreply.github.com>
2019-11-24 12:23:52 +0700

    Update Organization.java

--- Object/Organization.java
+++ Object/Organization.java
@@ -1,9 +1,16 @@
 public class Organization extends Kethuathuoctinhchung{
+	public String truSo;
 	public Organization (String dinhDanh,String nhan,String moTa) {
 		super();
 		this.dinhDanh=dinhDanh;
 		this.nhan=nhan;
 		this.moTa=moTa;
 		
+	}
+	public String getTruSo() {
+		return truSo;
+	}
+	public void setTruSo(String truSo) {
+		this.truSo = truSo;
 	}
 }
