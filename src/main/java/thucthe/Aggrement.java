25a076d973e390a68b01c675dda97c1594d45aa3
VanLong31199 <vanlong31199@gmail.com>
2020-04-08 18:21:40 +0700

    test

--- src/main/java/thucthe/Aggrement.java
+++ src/main/java/thucthe/Aggrement.java
@@ -5,6 +5,7 @@ public class Aggrement extends ThucThe{
 	public Aggrement(String dinhDanh, String nhanHienThi, String moTa) {
 		super(dinhDanh, nhanHienThi, moTa);
 		// TODO Auto-generated constructor stub
+		int x;
 	}
 
 	
